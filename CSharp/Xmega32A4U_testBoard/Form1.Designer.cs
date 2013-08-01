600ac63ecfab036c824dfb5838cd8720d47a06e8
dkflsrj@gmail.com <dkflsrj@gmail.com@47bb3af7-f548-4bf4-ff38-b33b44e1a720>
2013-08-01 16:15:25 +0000

    Доработка системы команд и отклика. Разблокирована SPI система DAC и ADC Улучшена система tracе'инга (добавлена кнопка "Подробный отчёт") Скорость USART поднята до 128000бод Установлена задержка приёма-передачи для компьютера - MC.delay = 3

--- CSharp/Xmega32A4U_testBoard/Form1.Designer.cs
+++ CSharp/Xmega32A4U_testBoard/Form1.Designer.cs
@@ -78,17 +78,9 @@
             this.BTN_LEDbyte = new System.Windows.Forms.Button();
             this.TXB_LEDbyte = new System.Windows.Forms.TextBox();
             this.BTN_COM_setMCwait = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.CHB_COM_DC_recieveDATA = new System.Windows.Forms.CheckBox();
-            this.CHB_COM_DC_sendDATA = new System.Windows.Forms.CheckBox();
-            this.CHB_COM_DC_recieveResponse = new System.Windows.Forms.CheckBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.BTN_COM_DC_send = new System.Windows.Forms.Button();
-            this.TXB_COM_DC_recieveDATA_count = new System.Windows.Forms.TextBox();
-            this.TXB_COM_DC_sendDATA = new System.Windows.Forms.TextBox();
-            this.TXB_COM_DC_command = new System.Windows.Forms.TextBox();
             this.Hinter = new System.Windows.Forms.ToolTip(this.components);
             this.CLK_timer = new System.Windows.Forms.Timer(this.components);
+            this.CHB_enableSuperTracer = new System.Windows.Forms.CheckBox();
             this.TABpanel.SuspendLayout();
             this.TABpanel_1.SuspendLayout();
             this.TABpanel_2.SuspendLayout();
@@ -96,7 +88,6 @@
             this.GRB_Counter.SuspendLayout();
             this.GRB_SPI.SuspendLayout();
             this.GRB_MC.SuspendLayout();
-            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // Log
@@ -214,7 +205,7 @@
             this.CMB_COM_BaudRate.Name = "CMB_COM_BaudRate";
             this.CMB_COM_BaudRate.Size = new System.Drawing.Size(121, 21);
             this.CMB_COM_BaudRate.TabIndex = 2;
-            this.CMB_COM_BaudRate.Text = "115200";
+            this.CMB_COM_BaudRate.Text = "128000";
             // 
             // CMB_COM_StopBits
             // 
@@ -317,7 +308,6 @@
             this.TABpanel_2.Controls.Add(this.GRB_Counter);
             this.TABpanel_2.Controls.Add(this.GRB_SPI);
             this.TABpanel_2.Controls.Add(this.GRB_MC);
-            this.TABpanel_2.Controls.Add(this.groupBox1);
             this.TABpanel_2.Location = new System.Drawing.Point(4, 22);
             this.TABpanel_2.Name = "TABpanel_2";
             this.TABpanel_2.Padding = new System.Windows.Forms.Padding(3);
@@ -502,7 +492,6 @@
             this.GRB_SPI.Controls.Add(this.TXB_DAC_voltage);
             this.GRB_SPI.Controls.Add(this.BTN_SPI_DAC_send);
             this.GRB_SPI.Controls.Add(this.BTN_SPI_ADC_request);
-            this.GRB_SPI.Enabled = false;
             this.GRB_SPI.Location = new System.Drawing.Point(6, 233);
             this.GRB_SPI.Name = "GRB_SPI";
             this.GRB_SPI.Size = new System.Drawing.Size(223, 81);
@@ -649,111 +638,30 @@
             this.BTN_COM_setMCwait.UseVisualStyleBackColor = true;
             this.BTN_COM_setMCwait.Click += new System.EventHandler(this.BTN_COM_setMCwait_Click);
             // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.CHB_COM_DC_recieveDATA);
-            this.groupBox1.Controls.Add(this.CHB_COM_DC_sendDATA);
-            this.groupBox1.Controls.Add(this.CHB_COM_DC_recieveResponse);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.BTN_COM_DC_send);
-            this.groupBox1.Controls.Add(this.TXB_COM_DC_recieveDATA_count);
-            this.groupBox1.Controls.Add(this.TXB_COM_DC_sendDATA);
-            this.groupBox1.Controls.Add(this.TXB_COM_DC_command);
-            this.groupBox1.Enabled = false;
-            this.groupBox1.Location = new System.Drawing.Point(340, 6);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(206, 140);
-            this.groupBox1.TabIndex = 4;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Прямое управление";
-            // 
-            // CHB_COM_DC_recieveDATA
-            // 
-            this.CHB_COM_DC_recieveDATA.AutoSize = true;
-            this.CHB_COM_DC_recieveDATA.Location = new System.Drawing.Point(6, 71);
-            this.CHB_COM_DC_recieveDATA.Name = "CHB_COM_DC_recieveDATA";
-            this.CHB_COM_DC_recieveDATA.Size = new System.Drawing.Size(113, 17);
-            this.CHB_COM_DC_recieveDATA.TabIndex = 5;
-            this.CHB_COM_DC_recieveDATA.Text = "Принять данные:";
-            this.CHB_COM_DC_recieveDATA.UseVisualStyleBackColor = true;
-            this.CHB_COM_DC_recieveDATA.CheckedChanged += new System.EventHandler(this.CHB_COM_DC_recieveDATA_CheckedChanged);
-            // 
-            // CHB_COM_DC_sendDATA
-            // 
-            this.CHB_COM_DC_sendDATA.AutoSize = true;
-            this.CHB_COM_DC_sendDATA.Location = new System.Drawing.Point(6, 45);
-            this.CHB_COM_DC_sendDATA.Name = "CHB_COM_DC_sendDATA";
-            this.CHB_COM_DC_sendDATA.Size = new System.Drawing.Size(113, 17);
-            this.CHB_COM_DC_sendDATA.TabIndex = 5;
-            this.CHB_COM_DC_sendDATA.Text = "Послать данные:";
-            this.CHB_COM_DC_sendDATA.UseVisualStyleBackColor = true;
-            this.CHB_COM_DC_sendDATA.CheckedChanged += new System.EventHandler(this.CHB_COM_DC_sendDATA_CheckedChanged);
-            // 
-            // CHB_COM_DC_recieveResponse
-            // 
-            this.CHB_COM_DC_recieveResponse.AutoSize = true;
-            this.CHB_COM_DC_recieveResponse.Location = new System.Drawing.Point(6, 95);
-            this.CHB_COM_DC_recieveResponse.Name = "CHB_COM_DC_recieveResponse";
-            this.CHB_COM_DC_recieveResponse.Size = new System.Drawing.Size(107, 17);
-            this.CHB_COM_DC_recieveResponse.TabIndex = 5;
-            this.CHB_COM_DC_recieveResponse.Text = "Принять отклик";
-            this.CHB_COM_DC_recieveResponse.UseVisualStyleBackColor = true;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 20);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(97, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Прямая команда:";
-            // 
-            // BTN_COM_DC_send
-            // 
-            this.BTN_COM_DC_send.Location = new System.Drawing.Point(127, 95);
-            this.BTN_COM_DC_send.Name = "BTN_COM_DC_send";
-            this.BTN_COM_DC_send.Size = new System.Drawing.Size(73, 39);
-            this.BTN_COM_DC_send.TabIndex = 1;
-            this.BTN_COM_DC_send.Text = "Послать";
-            this.BTN_COM_DC_send.UseVisualStyleBackColor = true;
-            this.BTN_COM_DC_send.Click += new System.EventHandler(this.BTN_DirectCommand_Click);
-            // 
-            // TXB_COM_DC_recieveDATA_count
-            // 
-            this.TXB_COM_DC_recieveDATA_count.Enabled = false;
-            this.TXB_COM_DC_recieveDATA_count.Location = new System.Drawing.Point(127, 69);
-            this.TXB_COM_DC_recieveDATA_count.Name = "TXB_COM_DC_recieveDATA_count";
-            this.TXB_COM_DC_recieveDATA_count.Size = new System.Drawing.Size(73, 20);
-            this.TXB_COM_DC_recieveDATA_count.TabIndex = 3;
-            this.Hinter.SetToolTip(this.TXB_COM_DC_recieveDATA_count, "Количество принимаемых байт");
-            // 
-            // TXB_COM_DC_sendDATA
-            // 
-            this.TXB_COM_DC_sendDATA.Enabled = false;
-            this.TXB_COM_DC_sendDATA.Location = new System.Drawing.Point(127, 43);
-            this.TXB_COM_DC_sendDATA.Name = "TXB_COM_DC_sendDATA";
-            this.TXB_COM_DC_sendDATA.Size = new System.Drawing.Size(73, 20);
-            this.TXB_COM_DC_sendDATA.TabIndex = 3;
-            this.Hinter.SetToolTip(this.TXB_COM_DC_sendDATA, "Данные задавать последовательно по байтам с пробелом после каждого (!) байта. При" +
-                    "мер: \"2 147 \"");
-            // 
-            // TXB_COM_DC_command
-            // 
-            this.TXB_COM_DC_command.Location = new System.Drawing.Point(127, 17);
-            this.TXB_COM_DC_command.Name = "TXB_COM_DC_command";
-            this.TXB_COM_DC_command.Size = new System.Drawing.Size(73, 20);
-            this.TXB_COM_DC_command.TabIndex = 2;
-            // 
             // CLK_timer
             // 
             this.CLK_timer.Interval = 2000;
             this.CLK_timer.Tick += new System.EventHandler(this.CLK_timer_Tick);
+            // 
+            // CHB_enableSuperTracer
+            // 
+            this.CHB_enableSuperTracer.AutoSize = true;
+            this.CHB_enableSuperTracer.Checked = true;
+            this.CHB_enableSuperTracer.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.CHB_enableSuperTracer.Location = new System.Drawing.Point(12, 362);
+            this.CHB_enableSuperTracer.Name = "CHB_enableSuperTracer";
+            this.CHB_enableSuperTracer.Size = new System.Drawing.Size(114, 17);
+            this.CHB_enableSuperTracer.TabIndex = 3;
+            this.CHB_enableSuperTracer.Text = "Подробный отчёт";
+            this.CHB_enableSuperTracer.UseVisualStyleBackColor = true;
+            this.CHB_enableSuperTracer.CheckedChanged += new System.EventHandler(this.CHB_enableSuperTracer_CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 562);
+            this.Controls.Add(this.CHB_enableSuperTracer);
             this.Controls.Add(this.TABpanel);
             this.Controls.Add(this.Log);
             this.MaximumSize = new System.Drawing.Size(600, 600);
@@ -772,9 +680,8 @@
             this.GRB_SPI.PerformLayout();
             this.GRB_MC.ResumeLayout(false);
             this.GRB_MC.PerformLayout();
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -801,20 +708,11 @@
         private System.Windows.Forms.Button BTN_MCstatus;
         private System.Windows.Forms.TextBox TXB_LEDbyte;
         private System.Windows.Forms.Button BTN_LEDbyte;
-        private System.Windows.Forms.TextBox TXB_COM_DC_command;
-        private System.Windows.Forms.Button BTN_COM_DC_send;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.CheckBox CHB_COM_DC_recieveDATA;
-        private System.Windows.Forms.CheckBox CHB_COM_DC_recieveResponse;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox TXB_COM_DC_sendDATA;
         private System.Windows.Forms.Button BTN_SPI_DAC_send;
         private System.Windows.Forms.Button BTN_SPI_ADC_request;
         private System.Windows.Forms.TextBox TXB_ADC_channel;
         private System.Windows.Forms.TextBox TXB_DAC_voltage;
         private System.Windows.Forms.TextBox TXB_DAC_channel;
-        private System.Windows.Forms.CheckBox CHB_COM_DC_sendDATA;
-        private System.Windows.Forms.TextBox TXB_COM_DC_recieveDATA_count;
         private System.Windows.Forms.Button BTN_COM_getMCversion;
         private System.Windows.Forms.Button BTN_COM_setMCwait;
         private System.Windows.Forms.Button BTN_COM_MC_CPUfreq;
@@ -840,6 +738,7 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox CHB_enableSuperTracer;
     }
 }
 
