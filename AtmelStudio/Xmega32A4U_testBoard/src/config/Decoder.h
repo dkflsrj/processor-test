48213b3e4d64bfe4111e162c459a272ddb8c9878
dkflsrj@gmail.com <dkflsrj@gmail.com@47bb3af7-f548-4bf4-ff38-b33b44e1a720>
2013-11-27 14:41:53 +0000

    C,C#: Переименования. Чистка. Оптимизация.

--- AtmelStudio/Xmega32A4U_testBoard/src/config/Decoder.h
+++ AtmelStudio/Xmega32A4U_testBoard/src/config/Decoder.h
@@ -5,14 +5,14 @@
 #ifndef Decoder
 #define Decoder
 //----------------------------------СПИСОК НОМЕРОВ SPI УСТРОЙСТВ-------------------------------------
-#define SPI_DEVICE_Number_DAC_IonSource				1
-#define SPI_DEVICE_Number_DAC_Detector				2
-#define SPI_DEVICE_Number_DAC_Inlet					3
+#define SPI_DEVICE_Number_DAC_PSIS					1
+#define SPI_DEVICE_Number_DAC_DPS					2
+#define SPI_DEVICE_Number_DAC_PSInl					3
 #define SPI_DEVICE_Number_DAC_Scaner				4
 #define SPI_DEVICE_Number_DAC_Condensator			5
-#define SPI_DEVICE_Number_ADC_IonSource				6
-#define SPI_DEVICE_Number_ADC_Detector				7
-#define SPI_DEVICE_Number_ADC_Inlet					8
+#define SPI_DEVICE_Number_ADC_PSIS					6
+#define SPI_DEVICE_Number_ADC_DPS					7
+#define SPI_DEVICE_Number_ADC_PSInl					8
 #define SPI_DEVICE_Number_ADC_MSV					9
 //-----------------------------------------СПИСОК КОМАНД---------------------------------------------
 #define COMMAND_MC_get_Version						1	//Команда: Запросить версию прошивки
@@ -21,9 +21,8 @@
 #define COMMAND_MC_reset							4	//Команда: Перезагрузка МК
 #define COMMAND_MC_wait								5	//Команда: Перевести МК в ожидание
 #define COMMAND_checkCommandStack					8	//Команда: Вернуть стэк команда (МК ведёт счёт команд с самого запуска)
-#define COMMAND_retransmitToTIC						11	//Команда: Ретранслировать данные насосу
-		
-#define COMMAND_LOCK								13	//Команда: Любая команда заканчивается этой (замок)
+
+#define COMMAND_LOCK								13	//Команда: Любая команда заканчивается этой (затвор)
 
 #define COMMAND_MC_get_Status						20  //Команда: Запросить состояние МК
 
@@ -31,27 +30,23 @@
 #define COMMAND_COUNTERS_stop						31	//Команда: Остановить счётчик
 #define COMMAND_COUNTERS_sendResults				32	//Команда: Послать результаты счёта
 //Команды DAC'ам
-#define COMMAND_IonSource_set_Voltage				40	//Команда: Задать напряжение DAC'у Ионного Источника
-#define COMMAND_Detector_set_Voltage				41	//Команда: Задать напряжение DAC'у Детектора
+#define COMMAND_PSIS_set_Voltage					40	//Команда: Задать напряжение DAC'у Ионного Источника
+#define COMMAND_DPS_set_Voltage						41	//Команда: Задать напряжение DAC'у Детектора
 #define COMMAND_Scaner_set_Voltage					42	//Команда: Задать напряжение DAC'у Сканера
 #define COMMAND_Condensator_set_Voltage				43	//Команда: Задать напряжение конденсатора
+#define COMMAND_PSInl_set_Voltage					44	//Команда: Задать напряжение натекателя
 
-#define COMMAND_Inlet_set_Voltage					49	//Команда: Задать напряжение натекателя
-#define COMMAND_Heater_set_Voltage					50	//Команда: Задать напряжение нагревателя
-
+#define COMMAND_retransmitToTIC						50	//Команда: Ретранслировать данные насосу
 
 #define COMMAND_KEY									58	//Команда: Любая команда начинается с этой (ключ)
 
 //Команды ADC'ам
-#define COMMAND_IonSource_get_Voltage				60	//Команда: Запросить напряжение у ADC Ионного Источника
-#define COMMAND_Detector_DV1_get_Voltage			64	//Команда: Запросить напряжение DV1
-#define COMMAND_Detector_DV2_get_Voltage			65	//Команда: Запросить напряжение DV2
-#define COMMAND_Detector_DV3_get_Voltage			66	//Команда: Запросить напряжение DV3
-#define COMMAND_Inlet_get_Voltage					67	//Команда: Запросить напряжение натекателя
-#define COMMAND_Heater_get_Voltage					68	//Команда: Запросить напряжение нагревателя
-#define COMMAND_MSV_get_Voltage						70	//Команда: Запросить напряжение конденсатора ("+" или "-") или сканера (родительское или сканирующее)
+#define COMMAND_PSIS_get_Voltage					60	//Команда: Запросить напряжение у ADC Ионного Источника
+#define COMMAND_DPS_get_Voltage						61	//Команда: Запросить напряжение DV1
+#define COMMAND_MSV_get_Voltage						62	//Команда: Запросить напряжение конденсатора ("+" или "-") или сканера (родительское или сканирующее)
+#define COMMAND_PSInl_get_Voltage					63	//Команда: Запросить напряжение натекателя
 
-#define COMMAND_Flags_set							80	//Команда: Установить флаги (SEMV1,SEMV2,SEMV3,SPUMP,iEDCD,iHVE)
+#define COMMAND_Flags_set							70	//Команда: Установить флаги (SEMV1,SEMV2,SEMV3,SPUMP,iEDCD,iHVE)
 //-----------------------------------------------LAM'ы-----------------------------------------------
 //ПОЯСНЕНИЯ: Асинхронные сообщения информирующие ПК о чём либо.
 //Метка
