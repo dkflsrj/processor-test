ddcf87fd229f6ecb1b78c89c7b4c4284dd27b2a3
dkflsrj <dkflsrj@gmail.com>
2013-12-18 11:44:55 +0000

    Восстановление после откатов. Добавление документации по TIC

--- AtmelStudio/Xmega32A4U_testBoard/src/main.c
+++ AtmelStudio/Xmega32A4U_testBoard/src/main.c
@@ -23,8 +23,8 @@
 
 //---------------------------------------ќѕ–≈ƒ≈Ћ≈Ќ»я----------------------------------------------
 //ћ 
-#define version										125
-#define birthday									20131216
+#define version										126
+#define birthday									20131218
 //—чЄтчики
 #define RTC_Status_ready							0		//—чЄтчики готов к работе
 #define RTC_Status_stopped							1		//—чЄтчики был принудительно остановлен
